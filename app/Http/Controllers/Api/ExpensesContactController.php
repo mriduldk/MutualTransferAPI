b292326008a7042669a76cf46e554ed2bd5deaf3
Mridul Das <mridul.das.9706@gmail.com>
2024-03-17 22:21:51 +0530

    use Illuminate\Support\Facades\DB; added

--- app/Http/Controllers/Api/ExpensesContactController.php
+++ app/Http/Controllers/Api/ExpensesContactController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\ExpensesContact;
 use \Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class ExpensesContactController extends Controller
 {
