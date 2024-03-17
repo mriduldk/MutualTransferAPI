cc8829c2305ca652ad1cf2ee21c61cdcf8204d45
Mridul Das <mridul.das.9706@gmail.com>
2024-03-17 13:04:04 +0530

    API to Api

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\AuthController;
-use App\Http\Controllers\API\GroupController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\GroupController;
 
 /*
 |--------------------------------------------------------------------------
