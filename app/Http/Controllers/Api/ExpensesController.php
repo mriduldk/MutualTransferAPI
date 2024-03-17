481e38a88af1b88a5aeaef1e500292025963d1a1
Mridul Das <mridul.das.9706@gmail.com>
2024-03-17 17:49:11 +0530

    bug in expenses controller

--- app/Http/Controllers/Api/ExpensesController.php
+++ app/Http/Controllers/Api/ExpensesController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Group;
+use App\Models\Expenses;
 
 
 class ExpensesController extends Controller
